49116389478fb9332a3b090ce9c2c467e52c1654
EriclesCalvt <ericlesprogrammer@gmail.com>
2023-05-02 07:59:49 -0300

    teste-back-end

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const app = require("./app");
 const loaders = require("./loaders");
 const cors = require("cors");
+const port = process.env.PORT || 3000
 
 
 
 app.use(cors());
 loaders.start();
 
-app.listen(process.env.PORT, () => {
+app.listen(port , () => {
   console.log("Server is running");
 });
